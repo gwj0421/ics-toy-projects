4cfae7de9d3b0e090ec77b6ee136304e851f7002
gunwoong <gwj0421@gmail.com>
2023-11-02 14:54:03 +0900

    feat : 중복되는 함수를 유틸함수로 정
    - 대문자를 기준으로 나눠지는 경우, 특수문자를 구분자로 나누고 나뉜다음 기존의 대문자를 소문자로 바꿈
    - 대문자를 기준으로 나눠지는 경우, 특수문자를 구분자로 나누고 나뉜다음 기존의 소문자를 대문자로 바꿈
    - 첫글자를 대문자로 바꿈
    - 첫글자를 소문자로 바꿈

--- src/main/java/com/example/ics/utils/StringUtil.java
+++ src/main/java/com/example/ics/utils/StringUtil.java
@@ -24,4 +24,53 @@ public class StringUtil {
     public static boolean checkUpperCase(char content) {
         return Character.isUpperCase(content);
     }
+
+    public static String convertDelimiterFromUpperCaseToLowerCaseAndCharacter(StringBuilder stringBuilder, String content, String delimiter) {
+        // 대문자를 기준으로 나눠지는 경우, 특수문자를 구분자로 나누고 나뉜다음 기존의 대문자를 소문자로 바꿈
+        for (int i = 1; i < content.length(); i++) {
+            char ch = content.charAt(i);
+            if (checkUpperCase(ch)) {
+                stringBuilder.append(delimiter);
+                stringBuilder.append(makeLowerCase(ch));
+            } else {
+                stringBuilder.append(ch);
+            }
+        }
+        String result = stringBuilder.toString();
+        stringBuilder.setLength(0);
+        return result;
+    }
+
+    public static String convertDelimiterFromUpperCaseToUpperCaseAndCharacter(StringBuilder stringBuilder, String content, String delimiter) {
+        // 대문자를 기준으로 나눠지는 경우, 특수문자를 구분자로 나누고 나뉜다음 기존의 소문자를 대문자로 바꿈
+        for (int i = 1; i < content.length(); i++) {
+            char ch = content.charAt(i);
+            if (checkUpperCase(ch)) {
+                stringBuilder.append(delimiter);
+                stringBuilder.append(ch);
+            } else {
+                stringBuilder.append(makeUpperCase(ch));
+            }
+        }
+        String result = stringBuilder.toString();
+        stringBuilder.setLength(0);
+        return result;
+    }
+
+
+    public static String makeFirstLetter2UpperCase(StringBuilder stringBuilder, String content) {
+        stringBuilder.append(content);
+        stringBuilder.setCharAt(0, makeUpperCase(stringBuilder.charAt(0)));
+        content = stringBuilder.toString();
+        stringBuilder.setLength(0);
+        return content;
+    }
+
+    public static String makeFirstCharacter2LowerCase(StringBuilder stringBuilder, String content) {
+        stringBuilder.append(content);
+        stringBuilder.setCharAt(0, makeLowerCase(stringBuilder.charAt(0)));
+        content = stringBuilder.toString();
+        stringBuilder.setLength(0);
+        return content;
+    }
 }
